5da02a2445a4c1d438289129bde789a73db44e1e
JrHero14 <Overload.emperor14@gmail.com>
2025-11-10 17:46:45 +0700

    Test walk

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
+from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtGui import QMovie, QCursor
+from PyQt5.QtCore import Qt, QTimer
+import sys
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = QApplication(sys.argv)
 
+label = QLabel()
+label.setWindowFlags(
+    Qt.FramelessWindowHint |
+    Qt.WindowStaysOnTopHint |
+    Qt.Tool
+)
+label.setAttribute(Qt.WA_TranslucentBackground)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+main_dir = "./assets/"
 
+# Load GIF
+movie = QMovie(main_dir + "walk-8.gif")
+label.setMovie(movie)
+movie.start()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+label.adjustSize()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Init Position
+screen = app.primaryScreen().geometry()
+label_width = label.width()
+label_height = label.height()
+
+x = (screen.width() - label_width) // 2
+y = screen.height() - label_height - 50
+label.move(x, y)
+label.show()
+
+speed = 5  # (1 = lambat, 10 = cepat)
+
+timer = QTimer()
+
+def follow_mouse():
+    global x, y
+    mouse_pos = QCursor.pos()
+
+    target_x = mouse_pos.x() - label_width // 2
+    target_y = mouse_pos.y() - label_height // 2
+
+    dx = target_x - x
+    dy = target_y - y
+    distance = math.hypot(dx, dy)
+
+    if distance < 1:
+        return
+
+    dir_x = dx / distance
+    dir_y = dy / distance
+
+    x += dir_x * speed
+    y += dir_y * speed
+
+    label.move(int(x), int(y))
+
+timer.timeout.connect(follow_mouse)
+timer.start(16)  # 60 FPS
+
+sys.exit(app.exec_())
